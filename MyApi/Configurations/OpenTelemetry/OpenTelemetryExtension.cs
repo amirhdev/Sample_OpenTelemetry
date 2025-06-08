c48ae2aaa3e11c0fed651b1db2957c2b527853d8
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-06-08 20:31:20 +0330

    Export trace data to Jaeger

--- MyApi/Configurations/OpenTelemetry/OpenTelemetryExtension.cs
+++ MyApi/Configurations/OpenTelemetry/OpenTelemetryExtension.cs
@@ -7,12 +7,13 @@ namespace MyApi.Configurations.OpenTelemetry;
 
 public static class OpenTelemetryExtension
 {
-    public static OpenTelemetryBuilder ConfigureTrace(this OpenTelemetryBuilder openTelemetryBuilder, string serviceName) 
+    public static OpenTelemetryBuilder ConfigureTrace(this OpenTelemetryBuilder openTelemetryBuilder, string serviceName)
         => openTelemetryBuilder.WithTracing(x => x
               .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
               .AddAspNetCoreInstrumentation()
               .AddHttpClientInstrumentation()
-              .AddConsoleExporter());
+              .AddConsoleExporter()
+              .AddOtlpExporter(x => x.Endpoint = new Uri("http://localhost:4317")));
 
     public static OpenTelemetryBuilder ConfigureMetric(this OpenTelemetryBuilder openTelemetryBuilder, string serviceName)
         => openTelemetryBuilder.WithMetrics(x => x
---
+++ MyApi/README.md
@@ -0,0 +1,7 @@
+﻿# Export trace data to Jaeger
+
+1. Run Jaeger container
+```bash
+docker run --name jaeger -p 13133:13133 -p 16686:16686 -p 4317:4317 -d --restart=unless-stopped jaegertracing/opentelemetry-all-in-one:latest
+```
+2. AddOtlpExporter
