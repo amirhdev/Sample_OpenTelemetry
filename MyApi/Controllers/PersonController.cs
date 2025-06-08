cc387cc9b133aff8b7022b0994f38b9c03f46c85
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-06-08 19:42:11 +0330

    Add "MyApi" project

---
+++ MyApi/Controllers/PersonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var people = new List<string>() { "Johnny Depp", "Tom Cruise" };
+        return Ok(people);
+    }
+}
